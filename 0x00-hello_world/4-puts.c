130b44074090af6a3c95d12eff75b43748f399a6
19mostafa92 <mostafa.kamel5negm@gmail.com>
2023-02-16 14:53:44 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print aline with puts fuction
+ * Return:0 (Succsse)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
